fd3acf91671ffbec3559f04b3c38ed187f4f6a80
Samuel Barksdale <samuelbarksdale@Samuels-MacBook-Air.local>
2019-05-14 10:59:48 -0400

    added db directory and connection file

--- index.js
+++ index.js
@@ -4,10 +4,6 @@ const app = express();
 app.use(express.json())
 app.use(require('./router'));
 
-// app.get('/', (req,res) => {
-//     res.send("the server is up")
-// })
-
 const PORT = process.env.PORT || 8000
 app.listen(PORT, () => {
     console.log("connected at: " + PORT)
