f36c1ac2e05e164e95f33dab79f33633a07e9a74
Samuel Barksdale <samuelbarksdale@Samuels-MacBook-Air.local>
2019-05-14 10:34:48 -0400

    started server

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json())
+// app.use(require('./routes'));
+
+app.get('/', (req,res) => {
+    res.send("the server is up")
+})
 
 const PORT = process.env.PORT || 8000
 app.listen(PORT, () => {
