4e874b5fee55427f85358728e7c16c12248f57b7
Samuel Barksdale <samuelbarksdale@Samuels-MacBook-Air.local>
2019-05-14 15:31:04 -0400

    added react code to index.js

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const app = express();
 app.use(express.json())
 app.use(require('./router'));
 
+app.use(express.static(__dirname + '/client/build/'));
+app.get('/', (req,res) => {
+    res.sendFile(__dirname + '/client/build/index.html')
+  })
+
 const PORT = process.env.PORT || 8000
 app.listen(PORT, () => {
     console.log("connected at: " + PORT)
