2c2ee0afd669ac08aa4b8011d3481ac196d7eeae
Samuel Barksdale <samuelbarksdale@Samuels-MacBook-Air.local>
2019-05-15 10:42:13 -0400

    updated routing

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const express = require('express');
 const app = express();
 
 app.use(express.json())
-app.use(require('./router'));
 
 app.use(express.static(__dirname + '/client/build/'));
 app.get('/', (req,res) => {
     res.sendFile(__dirname + '/client/build/index.html')
   })
+
+app.use(require('./router'));  
 
 const PORT = process.env.PORT || 8000
 app.listen(PORT, () => {
