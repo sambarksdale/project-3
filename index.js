f17298fe90e24b0d48f85bce97703a11a35b2022
Samuel Barksdale <samuelbarksdale@Samuels-MacBook-Air.local>
2019-05-15 10:56:04 -0400

    udated routing

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const express = require('express');
 const app = express();
 
 app.use(express.json())
+app.use(require('./router'));
 
 app.use(express.static(__dirname + '/client/build/'));
 app.get('/', (req,res) => {
     res.sendFile(__dirname + '/client/build/index.html')
   })
 
-app.use(require('./router'));  
+// app.use(require('./router'));  
 
 const PORT = process.env.PORT || 8000
 app.listen(PORT, () => {
