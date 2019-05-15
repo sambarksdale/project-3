6a6c8e3a61b4492a82ccd5a6ee163a1b1c311aa6
Samuel Barksdale <samuelbarksdale@Samuels-MacBook-Air.local>
2019-05-15 10:37:19 -0400

    updated router

--- router/index.js
+++ router/index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 
-router.get('/', (req,res) => {
-    res.send("this is the root")
-})
+// router.get('/', (req,res) => {
+//     res.send("this is the root")
+// })
 
 router.use(require('./user.js'))
 router.use(require('./login.js'))
